573757ef3af6ba855567a13b4a7f9a9249d41519
chengweiou <chengwei.ou.dora@gmail.com>
2019-06-15 17:27:57 -0700

    uat path

--- uat.sh
+++ uat.sh
@@ -1,7 +1,7 @@
 ./gradlew bootJar
-cp build/libs/triangulum-0.0.1-SNAPSHOT.jar ~/Desktop/docker/triangulum/ser.jar
-cp src/main/resources/application-uat.yml ~/Desktop/docker/triangulum/config/
-cp src/main/resources/log4j2.xml ~/Desktop/docker/triangulum/config/
-cd ~/Desktop/docker/triangulum
+cp build/libs/triangulum-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/triangulum/ser.jar
+cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/triangulum/config/
+cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/triangulum/config/
+cd ~/Desktop/docker/universe/triangulum
 docker stop triangulum
-docker run --rm -it -d --name triangulum -p 60003:8906 --network net -v /Users/chengweiou/Desktop/docker/triangulum:/proj/ -w /proj/ openjdk java -jar ser.jar
+docker run --rm -it -d --name triangulum -p 60003:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/triangulum:/proj/ -w /proj/ openjdk java -jar ser.jar

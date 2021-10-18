97b6f730eba7752b7bc89e023a9902847a3ba5b2
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-10-18 16:04:27 -0700

    不记得是啥了

--- uat.sh
+++ uat.sh
@@ -9,7 +9,3 @@ cd ~/Desktop/docker/universe/triangulum
 docker compose down
 docker compose rm -f
 docker compose up -d
-
-# cd ~/Desktop/docker/universe/triangulum
-# docker stop triangulum
-# docker run --rm -it -d --name triangulum -p 60003:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/triangulum:/proj/ -w /proj/ openjdk java -jar ser.jar

ebe74b13d378c73bcb37c2ced8a4189b1efaedd9
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-11-11 22:51:45 -0800

    add config

---
+++ src/main/java/chengweiou/universe/triangulum/base/config/ScheduleConfig.java
@@ -0,0 +1,10 @@
+package chengweiou.universe.triangulum.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+// todo tip: 使用时开启
+@Configuration
+// @EnableScheduling
+public class ScheduleConfig {
+}
